09b176b9c8ea7b8faa6f6463ea71643bdf1a64bd
OtomeSound <s890081tonyhsu@gmail.com>
2017-09-22 16:30:24 +0800

    Update options, reader and data 20170922

--- cardBase.js
+++ cardBase.js
@@ -9,7 +9,8 @@ var data_deck = {
   Special: [],
   Answer2: [],
   Special2: [],
-  Senzai: []
+  Senzai: [],
+  exCard: []
 };
 var data_source = {
   card: [],
@@ -19,93 +20,96 @@ var data_source = {
   Special2: {},
   Senzai: {}
 }
-var urlBase = "http://zh.nekowiz.wikia.com/api.php";
+var urlBase = 'http://zh.nekowiz.wikia.com/api.php';
 var queryParamsGap = {
-    format: "json",
-    action: "query",
-    prop: "revisions",
-    rvprop: "content",
-    generator: "allpages",
-    gapprefix: "Card/Data/",
-    gapnamespace: "10",
-    gaplimit: "50"
+    format: 'json',
+    action: 'query',
+    prop: 'revisions',
+    rvprop: 'content',
+    generator: 'allpages',
+    gapprefix: 'Card/Data/',
+    gapnamespace: '10',
+    gaplimit: '50'
 };
 var queryParamsP = {
-    format: "json",
-    action: "query",
-    prop: "revisions",
-    rvprop: "content",
-    titles: ""
+    format: 'json',
+    action: 'query',
+    prop: 'revisions',
+    rvprop: 'content',
+    titles: ''
 };
 function writeJSON(filename, source){
-  fs.writeFile("./json/"+filename+".json", JSON.stringify(source), function(err){
+  fs.writeFile('./json/'+filename+'.json', JSON.stringify(source), function(err){
     if(err){
       return console.log(err);
     }
   });
 }
 function querySourceC(start){
-    start = start || "";
+    start = start || '';
     var params = queryParamsGap;
-    params.gapprefix = "Card/Data/";
+    params.gapprefix = 'Card/Data/';
     if(start.length > 0) params.gapfrom = start;
     JSONP(urlBase, params, function(data){
-      var last = "";
-      if(typeof data["query-continue"] !== 'undefined') last = data["query-continue"].allpages.gapfrom;
+      var last = '';
+      if(typeof data['query-continue'] !== 'undefined') last = data['query-continue'].allpages.gapfrom;
       data_source.card.push(data);
-      if(data_source.card.length === data.length) console.log("Source progress: "+data_source.card.length);
+      if(data_source.card.length === data.length) console.log('Source progress: '+data_source.card.length);
       if(last.length <= 0) return sourceCHandler();
       else return querySourceC(last);
     });
 }
 function querySourceS(type){
     var params = queryParamsP;
-    if(type === 'Senzai') params.titles = "模板:Senzai/Data";
-    else params.titles = "模板:Skill/"+type+"/Data";
+    if(type === 'Senzai') params.titles = '模板:Senzai/Data';
+    else params.titles = '模板:Skill/'+type+'/Data';
     JSONP(urlBase, params, function(data){
       data_source[type] = data;
       return sourceSHandler(type);
     });
 }
 function queryCard(){
-    var str = "", str_s, group = {}, m;
+    var str = '', str_s, group = {}, m;
     var re = /\|(\w*)=(.*)/gmi;
     for(var ptr in data_source.card){
         for(var key in data_source.card[ptr].query.pages){
             if(key == -1) continue;
-            var id = data_source.card[ptr].query.pages[key].title.split("模板:Card/Data/")[1];
-            if(/^-?[\d.]+(?:e-?\d+)?$/.test(id) == false) continue;
+            var id = data_source.card[ptr].query.pages[key].title.split('模板:Card/Data/')[1];
+            if(/^(Ex)?\d+(-\d+)?$/.test(id) == false) continue;
             group.id = id;
             str = data_source.card[ptr].query.pages[key].revisions[0]['*'];
-            str_s = str.split("\n");
-            if(str_s[0] == "{{ Card/Data/{{{data}}}"){
-                for(var key in str_s){
-                    while((m = re.exec(str_s[key]))!= null){
-                        //console.log(m[1], m[2])
-                        group[m[1]] = m[2].trim();
-                    }
+            str_s = str.split('\n');
+            if(str_s[0] == '{{ Card/Data/{{{data}}}'){
+              for(var key in str_s){
+                while((m = re.exec(str_s[key]))!= null){
+                  //console.log(m[1], m[2])
+                  group[m[1]] = m[2].trim();
                 }
-                data_deck.card.push(group);
+              }
+              if(id.indexOf('Ex') !== -1){
+                group.id = group.id.replace('Ex', '');
+                data_deck.exCard.push(group);
+              }else data_deck.card.push(group);
             }
             group = {};
         };
     }
 }
 function querySkill(type){
-    var str = "", str_s, group = {}, m;
+    var str = '', str_s, group = {}, m;
     var re = /\|(\w*)=(.*)/gmi, re_h = /\|(.*)=\{\{/gmi;
     for(var key in data_source[type].query.pages){
       if(key == -1) continue;
       str = data_source[type].query.pages[key].revisions[0]['*'];
-      str_s = str.split("\n");
+      str_s = str.split('\n');
       var status = false, ptr = 0;
       for(var key in str_s){
-        if(str_s[key].indexOf("{{ #switch: {{{data}}}") != -1 && !status){
+        if(str_s[key].indexOf('{{ #switch: {{{data}}}') != -1 && !status){
           status = true;
           while((m = re_h.exec(str_s[key]))!= null){
             group['name'] = m[1];
           }
-        }else if(str_s[key].indexOf("}}") != -1 && status){
+        }else if(str_s[key].indexOf('}}') != -1 && status){
           status = false;
           ptr = 1;
           data_deck[type].push(group);
@@ -123,15 +127,20 @@ function querySkill(type){
 function sourceCHandler(){
     queryCard();
     data_deck.card = _.sortBy(data_deck.card, function(card){
-      var id = parseInt(card.id);
+      var id;
+      if(card.id.indexOf('-') !== -1){
+        var tmp = card.id.split('-').map(function(n) { return parseInt(n); });
+        id = tmp[0] + tmp[1] / 100;
+      }else id = parseInt(card.id);
       if(id < 0) return (900000-id);
       else return id;
     });
-    writeJSON("cardData", data_deck.card);
+    writeJSON('cardData', data_deck.card);
+    writeJSON('exCard', data_deck.exCard);
 }
 function sourceSHandler(type){
     querySkill(type);
-    writeJSON(type+"Skill", data_deck[type]);
+    writeJSON(type+'Skill', data_deck[type]);
 }
 
 querySourceS('Senzai');
--- src/scripts/cardBase.js
+++ src/scripts/cardBase.js
@@ -11,8 +11,8 @@ const cardBase = {
     };
   },
   callback() {},
-  urlBase: "http://zh.nekowiz.wikia.com/api.php",
-  urlSnapshot: "",
+  urlBase: 'http://zh.nekowiz.wikia.com/api.php',
+  urlSnapshot: '',
   data_deck: {
     card: [],
     Answer: [],
@@ -30,29 +30,29 @@ const cardBase = {
     Senzai: {}
   },
   queryParamsGap: {
-    format: "json",
-    action: "query",
-    prop: "revisions",
-    rvprop: "content",
-    generator: "allpages",
-    gapprefix: "Card/Data/",
-    gapnamespace: "10",
-    gaplimit: "50"
+    format: 'json',
+    action: 'query',
+    prop: 'revisions',
+    rvprop: 'content',
+    generator: 'allpages',
+    gapprefix: 'Card/Data/',
+    gapnamespace: '10',
+    gaplimit: '50'
   },
   queryParamsP: {
-    format: "json",
-    action: "query",
-    prop: "revisions",
-    rvprop: "content",
-    titles: ""
+    format: 'json',
+    action: 'query',
+    prop: 'revisions',
+    rvprop: 'content',
+    titles: ''
   },
-  querySourceC(start="") {
+  querySourceC(start='') {
       const params = this.queryParamsGap;
-      params.gapprefix = "Card/Data/";
+      params.gapprefix = 'Card/Data/';
       if(start.length > 0) params.gapfrom = start;
       jsonp(this.urlBase, params).then((data) => {
-        let last = "";
-        if(typeof data["query-continue"] !== 'undefined') last = data["query-continue"].allpages.gapfrom;
+        let last = '';
+        if(typeof data['query-continue'] !== 'undefined') last = data['query-continue'].allpages.gapfrom;
         this.data_source.card.push(data);
         console.log(`Source progress: ${this.data_source.card.length}`);
         if(last.length <= 0) return this.sourceCHandler();
@@ -61,51 +61,54 @@ const cardBase = {
   },
   querySourceS(type) {
       let params = this.queryParamsP;
-      if(type === 'Senzai') params.titles = "模板:Senzai/Data";
-      else params.titles = "模板:Skill/"+type+"/Data";
+      if(type === 'Senzai') params.titles = '模板:Senzai/Data';
+      else params.titles = '模板:Skill/'+type+'/Data';
       jsonp(this.urlBase, params).then((data) => {
         this.data_source[type] = data;
         return this.sourceSHandler(type);
       });
   },
   queryCard() {
-      let str = "", str_s, group = {}, m;
+      let str = '', str_s, group = {}, m;
       const re = /\|(\w*)=(.*)/gmi;
       for(const ptr in this.data_source.card){
           for(let key in this.data_source.card[ptr].query.pages){
               if(key == -1) continue;
-              const id = this.data_source.card[ptr].query.pages[key].title.split("模板:Card/Data/")[1];
-              if(/^-?[\d.]+(?:e-?\d+)?$/.test(id) == false) continue;
+              const id = this.data_source.card[ptr].query.pages[key].title.split('模板:Card/Data/')[1];
+              if(/^(Ex)?\d+(-\d+)?$/.test(id) == false) continue;
               group.id = id;
               str = this.data_source.card[ptr].query.pages[key].revisions[0]['*'];
-              str_s = str.split("\n");
-              if(str_s[0] == "{{ Card/Data/{{{data}}}"){
-                  for(let key in str_s){
-                      while((m = re.exec(str_s[key]))!= null){
-                          group[m[1]] = m[2];
-                      }
+              str_s = str.split('\n');
+              if(str_s[0] == '{{ Card/Data/{{{data}}}'){
+                for(let key in str_s){
+                  while((m = re.exec(str_s[key]))!= null){
+                    group[m[1]] = m[2];
                   }
-                  this.data_deck.card.push(group);
+                }
+                if(id.indexOf('Ex') !== -1){
+                  group.id = group.id.replace('Ex');
+                  data_deck.exCard.push(group);
+                }else data_deck.card.push(group);
               }
               group = {};
           };
       }
   },
   querySkill(type){
-      let str = "", str_s, group = {}, m;
+      let str = '', str_s, group = {}, m;
       let re = /\|(\w*)=(.*)/gmi, re_h = /\|(\S*)=\{\{/gmi;
       for(let key in this.data_source[type].query.pages){
         if(key == -1) continue;
         str = this.data_source[type].query.pages[key].revisions[0]['*'];
-        str_s = str.split("\n");
+        str_s = str.split('\n');
         let status = false, ptr = 0;
         for(let key in str_s){
-          if(str_s[key].indexOf("{{ #switch: {{{data}}}") != -1 && !status){
+          if(str_s[key].indexOf('{{ #switch: {{{data}}}') != -1 && !status){
             status = true;
             while((m = re_h.exec(str_s[key]))!= null){
               group['name'] = m[1];
             }
-          }else if(str_s[key].indexOf("}}") != -1 && status){
+          }else if(str_s[key].indexOf('}}') != -1 && status){
             status = false;
             ptr = 1;
             this.data_deck[type].push(group);
@@ -124,7 +127,11 @@ const cardBase = {
     source = source || '';
     if(source != 'snapshot') this.queryCard();
     this.data_deck.card = _.sortBy(this.data_deck.card, card => {
-      const id = parseInt(card.id);
+      let id;
+      if(card.id.indexOf('-') !== -1){
+        var tmp = card.id.split('-').map(function(n) { return parseInt(n); });
+        id = tmp[0] + tmp[1] / 100;
+      }else id = parseInt(card.id);
       if(id < 0) return (900000-id);
       else return id;
     });
