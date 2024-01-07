6028aea200c9e08f50d59026009f4c6cba3019f1
OtomeSound <s890081tonyhsu@gmail.com>
2024-01-07 14:22:12 +0800

    chore: add polyfill for legacy browsers

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), visualizer({
-    gzipSize: true,
-    brotliSize: true,
-    emitFile: false,
-    filename: 'rollupStats.html',
-    open: true,
-  })],
+  plugins: [
+    react(),
+    legacy({
+      renderLegacyChunks: false,
+      modernPolyfills: true,
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: 'rollupStats.html',
+      open: true,
+    })],
 });
