bdae3cfbc1479f5f1ac799b2e1161fd4eea141a4
OtomeSound <s890081tonyhsu@gmail.com>
2025-03-30 15:15:23 +0800

    feat: enable randInt for wiki images
    NOTE: Some images still not work when available in wiki.

--- src/components/WikiImage/utils.js
+++ src/components/WikiImage/utils.js
@@ -3,9 +3,9 @@ import md5Hex from 'md5-hex';
 export const twFilenameFix = (filename) => (filename.length !== 0 ? filename[0].toUpperCase() + filename.slice(1) : '');
 
 export const linkGenerator = (filename) => {
-  // const randInt = Math.floor(Math.random() * 5) + 1;
+  const randInt = Math.floor(Math.random() * 5) + 1;
   const md5name = md5Hex(filename);
-  return 'https://vignette.wikia.nocookie.net/nekowiz/images/'
+  return `https://vignette${randInt}.wikia.nocookie.net/nekowiz/images/`
   + `${md5name.charAt(0)}/${md5name.charAt(0)}${md5name.charAt(1)}/`
   + `${filename}/revision/latest?path-prefix=zh`;
 };
